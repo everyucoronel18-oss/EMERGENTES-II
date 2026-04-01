cb28277c8822adb4ba144c2e143287fb47804e55
Ever Yujra <everyucoronel18@gmail.com>
2026-03-31 20:56:19 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
 #Nombre: Ever Yujra Coronel RU:200028275
-from flask import Flask
-
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/usuario/<nombre>')
-def perfil_usuario(nombre):
-    return f'Perfil de: <strong>{nombre}</strong>'
-@app.route('/post/<id>')
-def mostrar_post(id):
-    return f'mostrando el post: <strong>{id}</strong>'
+tareas=[
+    {'id': 1, 'tarea': 'Apredender Flask', 'completada': False},
+    {'id': 2, 'tarea': 'Practicar con Flask', 'completada': False},
+]
+#GET-Obtener todas las tareas
+@app.route('/api/tareas', methods=['GET'])
+def listar_tareas():
+    return jsonify(tareas)
+#GET-Obtener una tarea especifica por ID
+@app.route('/api/tareas/<int:tarea_id>', methods=['GET'])
+def obtener_tarea(tarea_id):
+    tarea = None
+    for t in tareas:
+        if t['id'] == tarea_id:
+            tarea = t
+            break
+    if tarea:
+        return jsonify(tarea)
+    return jsonify({'error': 'Tarea no encontrada'}), 404
 
-@app.route('/categoria/<categoria>/<producto>')
-def productos(categoria, producto):
-    return f'Categoria: <strong>{categoria}</strong>, Producto: <strong>{producto}</strong>'
-
+#post-crear una nueva tarea
+@app.route('/api/tareas', methods=['POST'])
+def crear_tarea():
+    nueva_tarea = {
+        'id': len(tareas) + 1,
+        'tarea': request.json.get('tarea', ''),
+        'completada': request.json.get('completada', False)
+    }
+    tareas.append(nueva_tarea)
+    return jsonify(nueva_tarea), 201
+#eliminar una tarea por ID
+@app.route('/api/tareas/<int:tarea_id>', methods=['DELETE'])
+def eliminar_tarea(tarea_id):
+    global tareas
+    tareas = [t for t in tareas if t['id'] != tarea_id]
+    return jsonify({'mensaje': 'Tarea eliminada'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
